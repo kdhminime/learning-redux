de8b801b81ea278fb2b73ef566d4904a9bb49254
Dohyun Kim <kdhminime@gmail.com>
2023-01-25 15:40:54 -0700

    node modules update

--- src/index.js
+++ src/index.js
@@ -4,12 +4,32 @@ const plusButton = document.querySelector("#plus");
 const minusButton = document.querySelector("#minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+const plus = "plus"
+const minus = "minus";
 
-const countModifier = (state) => {
+
+const countModifier = (count = 0, action) => {
+
+  switch(action.type){
+    case plus:
+      return count+1;
+    case minus:
+      return count-1;
+    default:
+      return count;
+  }
   
-  return 0;
 };
 
-const countStore = createStore(countModifier);
+const count = createStore(countModifier);
 
-console.log(countStore.getState());
+const onChange = () => {
+  number.innerText = count.getState();
+};
+
+count.subscribe(onChange);
+
+plusButton.addEventListener("click", ()=> count.dispatch({type: plus}));
+minusButton.addEventListener("click", ()=> count.dispatch({type: minus}));
+
