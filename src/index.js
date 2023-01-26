9b66c5c341c52f996139434e5e6528b9c2b1bed1
Dohyun Kim <kdhminime@gmail.com>
2023-01-25 20:46:59 -0700

    installed redux for react

--- src/index.js
+++ src/index.js
@@ -1,35 +1,5 @@
-import {createStore} from "redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-const plusButton = document.querySelector("#plus");
-const minusButton = document.querySelector("#minus");
-const number = document.querySelector("span");
-
-number.innerText = 0;
-const plus = "plus"
-const minus = "minus";
-
-
-const countModifier = (count = 0, action) => {
-
-  switch(action.type){
-    case plus:
-      return count+1;
-    case minus:
-      return count-1;
-    default:
-      return count;
-  }
-  
-};
-
-const count = createStore(countModifier);
-
-const onChange = () => {
-  number.innerText = count.getState();
-};
-
-count.subscribe(onChange);
-
-plusButton.addEventListener("click", ()=> count.dispatch({type: plus}));
-minusButton.addEventListener("click", ()=> count.dispatch({type: minus}));
-
+ReactDOM.render(<App></App>, document.getElementById("root"));
